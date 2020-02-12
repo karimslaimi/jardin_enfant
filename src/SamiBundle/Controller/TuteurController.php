5b00942aa116ad112ddd183e9704c5d5787956dc
saami291 <sami.krimi29@gmail.com>
2020-02-12 01:09:18 +0100

    Sami work fixed

--- src/SamiBundle/Controller/TuteurController.php
+++ src/SamiBundle/Controller/TuteurController.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace SamiBundle\Controller;
+namespace AppBundle\Controller;
 
 use AppBundle\Entity\Tuteur;
+use AppBundle\Form\TuteurType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Tuteur controller.
@@ -24,10 +25,36 @@ class TuteurController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
 
-        $tuteurs = $em->getRepository('AppBundle:Tuteur')->findAll();
+        $tuteurs = $em->getRepository(Tuteur::class)->findAll();
 
         return $this->render('@Sami/tuteur/index.html.twig', array(
             'tuteurs' => $tuteurs,
+        ));
+    }
+
+    /**
+     * Creates a new tuteur entity.
+     *
+     * @Route("/new", name="tuteur_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $tuteur = new Tuteur();
+        $form = $this->createForm(TuteurType::class, $tuteur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($tuteur);
+            $em->flush();
+
+            return $this->redirectToRoute('tuteur_show', array('id' => $tuteur->getId()));
+        }
+
+        return $this->render('@Sami/tuteur/new.html.twig', array(
+            'tuteur' => $tuteur,
+            'form' => $form->createView(),
         ));
     }
 
@@ -39,9 +66,72 @@ class TuteurController extends Controller
      */
     public function showAction(Tuteur $tuteur)
     {
+        $deleteForm = $this->createDeleteForm($tuteur);
 
-        return $this->render('@Sami/tuteur/show.html.twig', array(
+        return $this->render('tuteur/show.html.twig', array(
             'tuteur' => $tuteur,
+            'delete_form' => $deleteForm->createView(),
         ));
+    }
+
+    /**
+     * Displays a form to edit an existing tuteur entity.
+     *
+     * @Route("/{id}/edit", name="tuteur_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Tuteur $tuteur)
+    {
+        $deleteForm = $this->createDeleteForm($tuteur);
+        $editForm = $this->createForm(TuteurType::class, $tuteur);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('tuteur_edit', array('id' => $tuteur->getId()));
+        }
+
+        return $this->render('@Sami/tuteur/edit.html.twig', array(
+            'tuteur' => $tuteur,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a tuteur entity.
+     *
+     * @Route("/{id}", name="tuteur_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Tuteur $tuteur)
+    {
+        $form = $this->createDeleteForm($tuteur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($tuteur);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('tuteur_index');
+    }
+
+    /**
+     * Creates a form to delete a tuteur entity.
+     *
+     * @param Tuteur $tuteur The tuteur entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Tuteur $tuteur)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('tuteur_delete', array('id' => $tuteur->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
     }
 }
