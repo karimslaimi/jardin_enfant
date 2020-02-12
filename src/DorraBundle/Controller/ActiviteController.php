f5a5b55ed1f1f7d5288619e6e4fb0a2a0f1c8b62
dorra <dorra.kerro@gmail.com>
2020-02-12 17:42:38 +0100

    dorra commitet ll mara thenia

---
+++ src/DorraBundle/Controller/ActiviteController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace DorraBundle\Controller;
+
+use AppBundle\Entity\Activite;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Activite controller.
+ *
+ * @Route("activite")
+ */
+class ActiviteController extends Controller
+{
+    /**
+     * Lists all activite entities.
+     *
+     * @Route("/", name="activite_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $activites = $em->getRepository('AppBundle:Activite')->findAll();
+
+        return $this->render('@Dorra/activite/index.html.twig', array(
+            'activites' => $activites,
+        ));
+    }
+
+    /**
+     * Creates a new activite entity.
+     *
+     * @Route("/new", name="activite_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $activite = new Activite();
+        $form = $this->createForm('DorraBundle\Form\ActiviteType', $activite);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($activite);
+            $em->flush();
+
+            return $this->redirectToRoute('activite_show', array('id' => $activite->getId()));
+        }
+
+        return $this->render('@Dorra/activite/new.html.twig', array(
+            'activite' => $activite,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a activite entity.
+     *
+     * @Route("/{id}", name="activite_show")
+     * @Method("GET")
+     */
+    public function showAction(Activite $activite)
+    {
+        $deleteForm = $this->createDeleteForm($activite);
+
+        return $this->render('@Dorra/activite/show.html.twig', array(
+            'activite' => $activite,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing activite entity.
+     *
+     * @Route("/{id}/edit", name="activite_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Activite $activite)
+    {
+        $deleteForm = $this->createDeleteForm($activite);
+        $editForm = $this->createForm('DorraBundle\Form\ActiviteType', $activite);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('activite_edit', array('id' => $activite->getId()));
+        }
+
+        return $this->render('@Dorra/activite/edit.html.twig', array(
+            'activite' => $activite,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a activite entity.
+     *
+     * @Route("/{id}", name="activite_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Activite $activite)
+    {
+        $form = $this->createDeleteForm($activite);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($activite);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('activite_index');
+    }
+
+    /**
+     * Creates a form to delete a activite entity.
+     *
+     * @param Activite $activite The activite entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Activite $activite)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('activite_delete', array('id' => $activite->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
