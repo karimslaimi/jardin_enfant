ce09add5650c7b3b4577d3dd3d856a8a80013f21
feridchatti <chattiferid9@gmail.com>
2020-02-14 01:00:39 +0100

    ferid work almost done!!!!!!!!!!!!

--- src/FeridBundle/Form/AbonnementType.php
+++ src/FeridBundle/Form/AbonnementType.php
@@ -2,7 +2,11 @@
 
 namespace FeridBundle\Form;
 
+use AppBundle\Entity\Enfant;
+use AppBundle\Entity\Jardin;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +17,17 @@ class AbonnementType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('date')->add('type')->add('etat')->add('montant')->add('jardin')->add('enfant');
+        $builder->add('date')->add('type')->add('etat')->add('montant')->add('jardin', EntityType::class,[
+                'class' => Jardin::class,
+                'choice_label' => 'name',
+                'expanded' => false,
+                'multiple' => false
+            ])->add('enfant', EntityType::class,[
+            'class' => Enfant::class,
+            'choice_label' => 'nom',
+            'expanded' => false,
+            'multiple' => false
+        ]);
     }/**
      * {@inheritdoc}
      */
