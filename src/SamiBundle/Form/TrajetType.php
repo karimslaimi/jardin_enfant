9311f7a0d97453cf6502c58ecbfbf3791079a961
saami291 <sami.krimi29@gmail.com>
2020-02-17 02:29:57 +0100

    added new controller(trajet)

---
+++ src/SamiBundle/Form/TrajetType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SamiBundle\Form;
+
+use AppBundle\Entity\Chauffeur;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+
+class TrajetType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('adresse')->add('heure')->add('chauffeur',EntityType::class,[
+            'class'=>Chauffeur::class,
+            'query_builder' => function (EntityRepository $er) {
+                return $er->createQueryBuilder('u')
+                    ->where('u.jardin=1');
+            },
+            'choice_label'=>'nom',
+            'multiple'=>false
+        ]);
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Trajet'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_trajet';
+    }
+
+
+}
