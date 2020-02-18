20969e2925fe9da98c02826bf7d8639dcabf551a
mohamed karim sleimi <karim-nar@live.fr>
2020-02-18 22:32:03 +0100

    fixing template a lot still to come

--- src/FeridBundle/Controller/DefaultController.php
+++ src/FeridBundle/Controller/DefaultController.php
@@ -7,11 +7,5 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     */
-    public function indexAction()
-    {
-        return $this->render('FeridBundle:Default:index.html.twig');
-    }
+
 }
